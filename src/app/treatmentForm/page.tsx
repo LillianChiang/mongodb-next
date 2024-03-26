c563274ebfd35c1aaa82d4307eaed57e01be2ec3
Lillian Chiang <lillianchiang0212@gmail.com>
2024-03-26 19:34:45 +1100

    feat: Add buttons in treatmentForm

--- src/app/treatmentForm/page.tsx
+++ src/app/treatmentForm/page.tsx
@@ -4,7 +4,6 @@ import { useState, ChangeEvent } from 'react';
 import { useRouter } from 'next/navigation';
 import { makeStyles } from '@material-ui/core/styles';
 import AddProducts from './AddProducts';
-import Navbar from '../components/DefaultLayout';
 
 import {
   Button,
@@ -82,138 +81,151 @@ const AddTreatmentRecord = () => {
   };
 
   return (
-    <Navbar>
-      {' '}
-      <Container>
-        <div className={classes.form}>
-          <Typography variant="h5">治療登錄 (新增)</Typography>
-          <form>
-            <Grid container spacing={2}>
-              <Grid item xs={2}>
-                <TextField
-                  fullWidth
-                  type="date"
-                  id="date"
-                  value={date}
-                  onChange={handleDateChange}
-                />
-              </Grid>
-              <Grid item>
-                <Button type="submit" variant="contained" color="primary">
-                  儲存
-                </Button>
-                <Button
-                  variant="contained"
-                  color="default"
-                  onClick={handleCancel}
+    <Container>
+      <div className={classes.form}>
+        <Typography variant="h5">治療登錄 (新增)</Typography>
+        <form>
+          <Grid container spacing={5}>
+            <Grid item xs={2}>
+              <TextField
+                fullWidth
+                type="date"
+                id="date"
+                value={date}
+                onChange={handleDateChange}
+              />
+            </Grid>
+            {/* {display login data} */}
+            <Grid item xs={8}>
+              <Button type="submit" variant="contained" color="primary">
+                收據: 0
+              </Button>
+              <Button
+                variant="contained"
+                color="default"
+                onClick={handleCancel}
+              >
+                刪除
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2} className={classes.marginBottom}>
+            <Grid item xs={4}>
+              <FormControl fullWidth>
+                <InputLabel id="therapist-label">治療師</InputLabel>
+                <Select
+                  labelId="therapist-label"
+                  id="therapist"
+                  value={therapist}
+                  onChange={handleTherapistChange}
                 >
-                  取消
-                </Button>
-              </Grid>
+                  <MenuItem value="therapist1">Therapist 1</MenuItem>
+                  <MenuItem value="therapist2">Therapist 2</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
-            <Grid container spacing={2} className={classes.marginBottom}>
-              <Grid item xs={4}>
-                <FormControl fullWidth>
-                  <InputLabel id="therapist-label">治療師</InputLabel>
-                  <Select
-                    labelId="therapist-label"
-                    id="therapist"
-                    value={therapist}
-                    onChange={handleTherapistChange}
-                  >
-                    <MenuItem value="therapist1">Therapist 1</MenuItem>
-                    <MenuItem value="therapist2">Therapist 2</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-              <Grid item xs={4}>
-                <FormControl fullWidth>
-                  <InputLabel id="treatmentType-label">治療項目</InputLabel>
-                  <Select
-                    labelId="treatmentType-label"
-                    id="treatmentType"
-                    value={treatmentType}
-                    onChange={(e) => setTreatmentType(e.target.value)}
-                  >
-                    <MenuItem value="type1">評估+衛教運動</MenuItem>
-                    <MenuItem value="type2">Type 2</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
+            <Grid item xs={4}>
+              <FormControl fullWidth>
+                <InputLabel id="treatmentType-label">治療項目</InputLabel>
+                <Select
+                  labelId="treatmentType-label"
+                  id="treatmentType"
+                  value={treatmentType}
+                  onChange={(e) => setTreatmentType(e.target.value)}
+                >
+                  <MenuItem value="type1">評估+衛教運動</MenuItem>
+                  <MenuItem value="type2">Type 2</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
-            <TextField
-              fullWidth
-              id="content"
-              label="評估內容"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-              className={classes.marginBottom}
-            />
-            <TextField
-              fullWidth
-              id="treatmentContent"
-              label="治療內容"
-              value={treatmentContent}
-              onChange={(e) => setTreatmentContent(e.target.value)}
-              className={classes.marginBottom}
-            />
-            <Grid container spacing={2}>
-              <Grid item xs={3}>
-                <TextField
-                  id="treatmentFee"
-                  label="治療費用 (現金)"
-                  value={treatmentFee}
-                  onChange={(e) => setTreatmentFee(e.target.value)}
-                  className={classes.marginBottom}
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <FormControlLabel
-                  control={
-                    <Switch
-                      id="openPreCharge"
-                      checked={openPreCharge}
-                      onChange={handleOpenPreChargeChange}
-                    />
-                  }
-                  label="是否開啟預收實現"
-                  className={classes.marginBottom}
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <TextField
-                  id="chargeFee"
-                  label="治療費用 (預收實現)"
-                  value={chargeFee}
-                  onChange={(e) => setChargeFee(e.target.value)}
-                  className={classes.marginBottom}
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <TextField
-                  id="currentTreatmentContent"
-                  label="預收款項"
-                  value={currentTreatmentContent}
-                  onChange={(e) => setCurrentTreatmentContent(e.target.value)}
-                  className={classes.marginBottom}
-                />
-              </Grid>
+          </Grid>
+          <TextField
+            fullWidth
+            id="content"
+            label="評估內容"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className={classes.marginBottom}
+          />
+          <TextField
+            fullWidth
+            id="treatmentContent"
+            label="治療內容"
+            value={treatmentContent}
+            onChange={(e) => setTreatmentContent(e.target.value)}
+            className={classes.marginBottom}
+          />
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <TextField
+                id="treatmentFee"
+                label="治療費用 (現金)"
+                value={treatmentFee}
+                onChange={(e) => setTreatmentFee(e.target.value)}
+                className={classes.marginBottom}
+              />
             </Grid>
-            <TextField
-              fullWidth
-              id="chargeNotes"
-              label="預收實現備註"
-              multiline
-              rows={4}
-              value={chargeNotes}
-              onChange={(e) => setChargeNotes(e.target.value)}
-              className={classes.marginBottom}
-            />
-          </form>
-        </div>
-        <AddProducts />
-      </Container>
-    </Navbar>
+            <Grid item xs={3}>
+              <FormControlLabel
+                control={
+                  <Switch
+                    id="openPreCharge"
+                    checked={openPreCharge}
+                    onChange={handleOpenPreChargeChange}
+                  />
+                }
+                label="是否開啟預收實現"
+                className={classes.marginBottom}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                id="chargeFee"
+                label="治療費用 (預收實現)"
+                value={chargeFee}
+                onChange={(e) => setChargeFee(e.target.value)}
+                className={classes.marginBottom}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                id="currentTreatmentContent"
+                label="預收款項"
+                value={currentTreatmentContent}
+                onChange={(e) => setCurrentTreatmentContent(e.target.value)}
+                className={classes.marginBottom}
+              />
+            </Grid>
+          </Grid>
+          <TextField
+            fullWidth
+            id="chargeNotes"
+            label="預收實現備註"
+            multiline
+            rows={4}
+            value={chargeNotes}
+            onChange={(e) => setChargeNotes(e.target.value)}
+            className={classes.marginBottom}
+          />
+          <Grid
+            item
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            xs={12}
+          >
+            <Button type="submit" variant="contained" color="primary">
+              儲存
+            </Button>
+            <Button variant="contained" color="default" onClick={handleCancel}>
+              取消
+            </Button>
+          </Grid>
+        </form>
+      </div>
+      <AddProducts />
+    </Container>
   );
 };
 
